c9fcabbab0346fbd2570ebaa289e6c4e61f85823
1disrupter <108752632+1disrupter@users.noreply.github.com>
2026-04-28 19:39:28 +0100

    Remove VenueCard exports from index.js
    Removed VenueHeroCard and VenueListItem exports from index.js.

--- frontend/src/components/v2n/index.js
+++ frontend/src/components/v2n/index.js
@@ -2,7 +2,7 @@ export { Logo, LogoMark } from "./Logo";
 export { Button, IconButton } from "./Button";
 export { Chip, BannerChip } from "./Chip";
 export { VibeScoreBadge, StatusIndicator } from "./VibeScore";
-export { VenueHeroCard, VenueListItem } from "./VenueCard";
+
 export { Modal } from "./Modal";
 export { ToastProvider, useToast } from "./Toast";
 export { Input, SearchBar, Select, Slider } from "./Input";
