31510ca6907e81fb5f2053bf50d1380e20600bcc
1disrupter <108752632+1disrupter@users.noreply.github.com>
2026-05-01 14:20:36 +0100

    Add disclaimer link to Footer component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
-    <div className="text-center text-xs text-gray-400 py-6">
+ <div className="text-center text-xs text-gray-400 py-6">
       © 2026 Vibe2Nite. All rights reserved. Martin Maughan
+      <a href="/disclaimer" className="underline">Disclaimer</a>
     </div>
   );
-}
+}   
+    
