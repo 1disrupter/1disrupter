40778f6763f1fa676ec8c0584df8f40268f79edc
1disrupter <108752632+1disrupter@users.noreply.github.com>
2026-05-01 14:02:55 +0100

    Add Footer component with copyright information

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="text-center text-xs text-gray-400 py-6">
+      © 2026 Vibe2Nite. All rights reserved. Martin Maughan
+    </div>
+  );
+}
